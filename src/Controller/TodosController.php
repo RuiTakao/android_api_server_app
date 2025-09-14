70be82555c0d716a4a4b4702942b473b15ff562a
kimuratakuou <ruia1082halfnc@gmail.com>
2025-09-14 11:49:42 +0900

    Todoデータ取得メソッド作成

--- src/Controller/TodosController.php
+++ src/Controller/TodosController.php
@@ -35,6 +35,32 @@ class TodosController extends AppController
         return $this->response;
     }
 
+    public function todo($id)
+    {
+        Log::debug(print_r($id, true));
+
+        $data = $this->Todos->find()
+            ->select([
+                'id',
+                'title',
+                'content',
+                'done' => 'is_done',
+                'deviceId' => 'device_id',
+                'createdAt' => 'created_at'
+            ])
+            ->where(["id" => $id])
+            ->first();
+
+        // Log::debug(print_r($data[1], true));
+        // $data = $data;
+
+        $this->response = $this->response
+            ->withType('application/json')
+            ->withStringBody(json_encode($data, JSON_UNESCAPED_UNICODE));
+
+        return $this->response;
+    }
+
     public function create()
     {
         $this->autoRender = false;
